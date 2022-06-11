b3c6c0e82c25d0e8c2c79488249a614db6eb4c68
Nicolas Barrios <nbarrios1337@gmail.com>
2022-06-11 14:18:27 -0400

    Introduced image output

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,28 @@
+use utils::clamp;
+
+mod utils;
+
+const IMAGE_W: u32 = 256;
+const IMAGE_H: u32 = 256;
+
+fn coords_to_image(x: u32, y: u32) -> image::Rgb<u8> {
+    let r: f64 = x as f64 / (IMAGE_W - 1) as f64;
+    let g: f64 = (IMAGE_H - y) as f64 / (IMAGE_H - 1) as f64;
+    let b = 0.25;
+
+    let ir = (256.0 * clamp(r, 0.0, 1.0)) as u8;
+    let ig = (256.0 * clamp(g, 0.0, 1.0)) as u8;
+    let ib = (256.0 * clamp(b, 0.0, 1.0)) as u8;
+
+    image::Rgb([ir, ig, ib])
+}
+
 fn main() {
+    let img_buf: image::RgbImage = image::ImageBuffer::from_fn(IMAGE_W, IMAGE_H, coords_to_image);
+
+    if let Err(why) = img_buf.save("image.png") {
+        eprintln!("Failed to write: {}", why);
+    }
+
     println!("Hello, world!");
 }
---
+++ src/utils.rs
@@ -0,0 +1,9 @@
+pub fn clamp<T: PartialOrd>(v: T, min: T, max: T) -> T {
+    if v < min {
+        min
+    } else if v > max {
+        max
+    } else {
+        v
+    }
+}
