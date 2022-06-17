75a75be64023f7948eb6adf0921ff7cce8f74e35
Nicolas Barrios <nbarrios1337@gmail.com>
2022-06-17 13:22:29 -0400

    some unit tests

--- src/vec3.rs
+++ src/vec3.rs
@@ -120,3 +120,124 @@ impl DivAssign<f32> for Vec3f32 {
     }
 }
 
+#[cfg(test)]
+mod tests {
+    use super::Vec3f32;
+
+    // use super::Vec3f32::*;
+    #[test]
+    fn new() {
+        let v = Vec3f32::new();
+        assert_eq!(v.x, 0.0);
+        assert_eq!(v.y, 0.0);
+        assert_eq!(v.z, 0.0);
+    }
+
+    #[test]
+    fn neg() {
+        let v = Vec3f32 {
+            x: 1.0,
+            y: 2.0,
+            z: 3.0,
+        };
+
+        assert_eq!(
+            -v,
+            Vec3f32 {
+                x: -1.0,
+                y: -2.0,
+                z: -3.0,
+            }
+        );
+    }
+
+    #[test]
+    fn add_vec() {
+        let a = Vec3f32 {
+            x: 1.0,
+            y: 2.0,
+            z: 3.0,
+        };
+
+        let b = Vec3f32 {
+            x: 3.0,
+            y: 2.0,
+            z: 1.0,
+        };
+
+        assert_eq!(
+            a + b,
+            Vec3f32 {
+                x: 4.0,
+                y: 4.0,
+                z: 4.0,
+            }
+        );
+    }
+
+    #[test]
+    fn sub_vec() {
+        let a = Vec3f32 {
+            x: 5.0,
+            y: 6.0,
+            z: 7.0,
+        };
+
+        let b = Vec3f32 {
+            x: 3.0,
+            y: 4.0,
+            z: 5.0,
+        };
+
+        assert_eq!(
+            a - b,
+            Vec3f32 {
+                x: 2.0,
+                y: 2.0,
+                z: 2.0,
+            }
+        );
+    }
+
+    #[test]
+    fn mul_vec() {
+        let a = Vec3f32 {
+            x: 2.0,
+            y: 3.0,
+            z: 4.0,
+        };
+
+        let b = Vec3f32 {
+            x: 2.0,
+            y: 3.0,
+            z: 4.0,
+        };
+
+        assert_eq!(
+            a * b,
+            Vec3f32 {
+                x: 4.0,
+                y: 9.0,
+                z: 16.0,
+            }
+        );
+    }
+
+    #[test]
+    fn div_vec() {
+        let a = Vec3f32 {
+            x: 4.0,
+            y: 6.0,
+            z: 8.0,
+        };
+
+        assert_eq!(
+            a / 2.0,
+            Vec3f32 {
+                x: 2.0,
+                y: 3.0,
+                z: 4.0,
+            }
+        );
+    }
+}
