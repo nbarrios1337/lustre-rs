68fc50096ea56345c2c114bc1add06f09aa8022b
Nicolas Barrios <nbarrios1337@gmail.com>
2022-06-21 08:58:42 -0400

    coords_to_image -> coord_to_pixel

--- src/main.rs
+++ src/main.rs
@@ -8,7 +8,7 @@ mod utils;
 const IMAGE_W: u32 = 256;
 const IMAGE_H: u32 = 256;
 
-fn coords_to_image(x: u32, y: u32) -> image::Rgb<u8> {
+fn coord_to_pixel(x: u32, y: u32) -> image::Rgb<u8> {
     let r: f64 = x as f64 / (IMAGE_W - 1) as f64;
     let g: f64 = (IMAGE_H - y) as f64 / (IMAGE_H - 1) as f64;
     let b = 0.25;
@@ -26,7 +26,7 @@ fn main() {
     let output_file = cli_args.output;
 
     // write image to file
-    let img_buf: image::RgbImage = image::ImageBuffer::from_fn(IMAGE_W, IMAGE_H, coords_to_image);
+    let img_buf: image::RgbImage = image::ImageBuffer::from_fn(IMAGE_W, IMAGE_H, coord_to_pixel);
     if let Err(why) = img_buf.save(output_file) {
         eprintln!("Failed to write: {}", why);
     }
