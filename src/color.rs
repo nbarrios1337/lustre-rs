0d95ea70c333882517b11f3ecfae9884412ac6ed
Nicolas Barrios <nbarrios1337@gmail.com>
2022-06-21 18:04:44 -0400

    Some color constants

---
+++ src/color.rs
@@ -0,0 +1,5 @@
+pub use glam::Vec3 as Color;
+
+pub const RED: Color = glam::const_vec3!([1.0, 0.0, 0.0]);
+pub const GREEN: Color = glam::const_vec3!([0.0, 1.0, 0.0]);
+pub const BLUE: Color = glam::const_vec3!([0.0, 0.0, 1.0]);
