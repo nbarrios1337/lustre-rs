21a54184322f4ad0c574def226178e6bcac8e24d
Nicolas Barrios <nbarrios1337@gmail.com>
2022-06-22 19:55:56 -0400

    Create list of hittables

--- src/hittable.rs
+++ src/hittable.rs
@@ -1,7 +1,10 @@
+use std::rc::Rc;
+
 use glam::Vec3;
 
 use crate::ray::Ray;
 
+#[derive(Debug)]
 pub struct HitRecord {
     pub point: Vec3,
     pub normal: Vec3,
@@ -23,4 +26,24 @@ impl HitRecord {
 
 pub trait Hittable {
     fn hit(&self, ray: &Ray, t_min: f32, t_max: f32) -> Option<HitRecord>;
+}
+
+pub struct HittableList {
+    pub hittables: Vec<Rc<dyn Hittable>>,
+}
+
+impl Hittable for HittableList {
+    fn hit(&self, ray: &Ray, t_min: f32, t_max: f32) -> Option<HitRecord> {
+        let mut rec: Option<HitRecord> = None;
+        let mut t_closest = t_max;
+
+        for hittable in self.hittables.iter() {
+            rec = hittable.hit(ray, t_min, t_closest);
+            if let Some(HitRecord { t, .. }) = rec {
+                t_closest = t;
+            }
+        }
+
+        rec
+    }
 }
