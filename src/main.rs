5d83c7b9feb97dd809bd036be8e815911e650ded
Nicolas Barrios <nbarrios1337@gmail.com>
2022-06-23 00:14:03 -0400

    More of the same should be same image

--- src/main.rs
+++ src/main.rs
@@ -1,6 +1,8 @@
 use camera::Camera;
 use cli::{Arguments, Parser};
 use glam::Vec3;
+use hittable::HittableList;
+use sphere::Sphere;
 
 mod camera;
 mod cli;
@@ -19,18 +21,38 @@ fn main() {
     let img_w = 400;
     let img_h = (img_w as f32 / Camera::ASPECT_RATIO) as u32;
 
+    let world: HittableList = HittableList(vec![
+        Box::new(Sphere {
+            center: Vec3::new(0.0, -100.5, -1.0),
+            radius: 100.0,
+        }),
+        Box::new(Sphere {
+            center: Vec3::new(0.0, 0.0, -1.0),
+            radius: 0.5,
+        }),
+        Box::new(Sphere {
+            center: Vec3::new(0.0, 0.0, -1.0),
+            radius: 0.5,
+        }),
+        Box::new(Sphere {
+            center: Vec3::new(0.0, -100.5, -1.0),
+            radius: 100.0,
+        }),
+    ]);
+
     // write image to file
     let img_buf: image::RgbImage =
         image::ImageBuffer::from_fn(img_w, img_h, |x: u32, y: u32| -> image::Rgb<u8> {
             let u: f64 = x as f64 / (img_w - 1) as f64;
             let v: f64 = (img_h - y) as f64 / (img_h - 1) as f64;
 
-            cam.get_ray(u as f32, v as f32).into()
+            let color = cam.get_ray(u as f32, v as f32).shade(&world);
+            color.into()
         });
     if let Err(why) = img_buf.save(output_file) {
         eprintln!("Failed to write: {}", why);
     }
 
     let v = Vec3::new(0.0, 0.0, 0.0);
-    println!("{}", v)
+    println!("{}", v);
 }
