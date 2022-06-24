bad08cfef06d4513ea482fd00ef67bcf548e4b74
Nicolas Barrios <nbarrios1337@gmail.com>
2022-06-23 23:37:24 -0400

    Some cleanup

--- src/main.rs
+++ src/main.rs
@@ -10,17 +10,18 @@ mod color;
 mod hittable;
 mod ray;
 mod sphere;
-mod utils;
 
 fn main() {
     // Parsing cli args
     let cli_args = Arguments::parse();
     let output_file = cli_args.output;
 
+    // Setup camera properties
     let cam = Camera::new();
     let img_w = 400;
     let img_h = (img_w as f32 / Camera::ASPECT_RATIO) as u32;
 
+    // Generate world objects
     let world: HittableList = HittableList(vec![
         Box::new(Sphere {
             center: Vec3::new(0.0, 0.0, -1.0),
@@ -32,19 +33,20 @@ fn main() {
         }),
     ]);
 
-    // write image to file
+    // Generate image
     let img_buf: image::RgbImage =
         image::ImageBuffer::from_fn(img_w, img_h, |x: u32, y: u32| -> image::Rgb<u8> {
             let u: f64 = x as f64 / (img_w - 1) as f64;
             let v: f64 = (img_h - y) as f64 / (img_h - 1) as f64;
 
-            let color = cam.get_ray(u as f32, v as f32).shade(&world);
-            color.into()
+            cam.get_ray(u as f32, v as f32).shade(&world).into()
         });
-    if let Err(why) = img_buf.save(output_file) {
-        eprintln!("Failed to write: {}", why);
-    }
 
-    let v = Vec3::new(0.0, 0.0, 0.0);
-    println!("{}", v);
+    // write image to file
+    match img_buf.save(output_file.clone()) {
+        Ok(()) => println!("Done! Image written to {:?}", output_file),
+        Err(why) => {
+            eprintln!("Failed to write: {}", why);
+        }
+    }
 }
--- src/utils.rs
+++
@@ -1,9 +0,0 @@
-pub fn clamp<T: PartialOrd>(v: T, min: T, max: T) -> T {
-    if v < min {
-        min
-    } else if v > max {
-        max
-    } else {
-        v
-    }
-}
