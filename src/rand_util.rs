b7ccabcf15e2d32b960790a18134348355e181c4
Nicolas Barrios <nbarrios1337@gmail.com>
2022-06-26 13:49:50 -0400

    more random gen for vec3

--- src/rand_util.rs
+++ src/rand_util.rs
@@ -1,7 +1,22 @@
+use glam::Vec3;
 use rand::Rng;
-
-pub use rand;
 
 pub fn rand_f32() -> f32 {
     rand::thread_rng().gen::<f32>()
+}
+
+pub fn rand_range_f32(min: f32, max: f32) -> f32 {
+    rand::thread_rng().gen_range(min..max)
+}
+
+pub fn rand_vec3() -> Vec3 {
+    rand::thread_rng().gen::<Vec3>()
+}
+
+pub fn rand_range_vec3(min: f32, max: f32) -> Vec3 {
+    Vec3::new(
+        rand_range_f32(min, max),
+        rand_range_f32(min, max),
+        rand_range_f32(min, max),
+    )
 }
