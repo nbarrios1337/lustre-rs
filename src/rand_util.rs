33c4f9d87d71fec23f7be7bebf62509c60333b5d
Nicolas Barrios <nbarrios1337@gmail.com>
2022-06-30 22:44:59 -0400

    Add documentation to rand_utils

--- src/rand_util.rs
+++ src/rand_util.rs
@@ -1,18 +1,24 @@
+//! Miscelleanous utilities related to random number generation and random sampling
+
 use glam::Vec3;
 use rand::Rng;
 
+/// Generates a random f32.
 pub fn rand_f32() -> f32 {
     rand::thread_rng().gen::<f32>()
 }
 
+/// Generates a random f32 within the given range `[min, max)`.
 pub fn rand_range_f32(min: f32, max: f32) -> f32 {
     rand::thread_rng().gen_range(min..max)
 }
 
+/// Generates a random [Vec3].
 pub fn rand_vec3() -> Vec3 {
     rand::thread_rng().gen::<Vec3>()
 }
 
+/// Generates a random [Vec3] within the given range `[min, max)`.
 pub fn rand_range_vec3(min: f32, max: f32) -> Vec3 {
     Vec3::new(
         rand_range_f32(min, max),
@@ -21,6 +27,10 @@ pub fn rand_range_vec3(min: f32, max: f32) -> Vec3 {
     )
 }
 
+/// Generates a random [Vec3] within the unit sphere (radius 1).
+///
+/// Uses [rand_range_vec3] to generate [Vec3]'s within [-1, 1),
+/// rejecting those who's squared length is greater than 1.
 pub fn rand_vec3_in_unit_sphere() -> Vec3 {
     loop {
         let v = rand_range_vec3(-1.0, 1.0);
@@ -30,6 +40,9 @@ pub fn rand_vec3_in_unit_sphere() -> Vec3 {
     }
 }
 
+/// Generates a random [Vec3] within the unit disk (radius 1).
+/// 
+/// Functionally similar to [rand_vec3_in_unit_sphere].
 pub fn rand_vec3_in_unit_disk() -> Vec3 {
     loop {
         let v = Vec3::new(rand_range_f32(-1.0, 1.0), rand_range_f32(-1.0, 1.0), 0.0);
