399ff702f89421f9042a8d5ea76394cd8392fe23
Nicolas Barrios <nbarrios1337@gmail.com>
2022-07-01 11:12:22 -0400

    Document the hittable module

--- src/hittable.rs
+++ src/hittable.rs
@@ -1,3 +1,6 @@
+//! Contains description of what it means to intersect something,
+//! as well as what's returned on intersection
+
 use std::{
     ops::{Deref, DerefMut},
     rc::Rc,
@@ -7,12 +10,18 @@ use glam::Vec3;
 
 use crate::{material::Material, ray::Ray};
 
+/// Defines a set of data returned upon a successful intersection
 #[derive(Debug)]
 pub struct HitRecord {
+    /// Point of intersection in 3D space
     pub point: Vec3,
+    /// Surface normal off the point of intersection
     pub normal: Vec3,
+    /// Material of the intersected object
     pub material: Rc<Material>,
+    /// distance from the origin to the point of intersection
     pub t: f32,
+    /// Whether or not the ray hit the object's inside or outside face
     pub front_face: bool,
 }
 
@@ -28,10 +37,15 @@ impl HitRecord {
     }
 }
 
+/// Describes the behavior of objects that support intersection
 pub trait Hittable {
+    /// Intersects the given ray with the object
+    /// 
+    /// Returns a `Some(HitRecord)` if successful, otherwise `None`
     fn hit(&self, ray: &Ray, t_min: f32, t_max: f32) -> Option<HitRecord>;
 }
 
+/// Wrapper newtype holding a [Vec] of types implementing the [Hittable] trait
 pub struct HittableList(pub Vec<Box<dyn Hittable>>);
 
 impl Deref for HittableList {
