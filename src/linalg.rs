b1b5a343ddaf8c617d75732891dd63a2e9e53949
Nicolas Barrios <nbarrios1337@gmail.com>
2022-06-30 23:51:43 -0400

    Document linalg module

--- src/linalg.rs
+++ src/linalg.rs
@@ -1,9 +1,17 @@
+//! Ray scattering utilities
 use glam::Vec3;
 
+/// Returns a reflected ray direction based on the given normal
+/// 
+/// Performs the following computation: `v - 2 * v.dot(n) * n`
 pub fn reflect(v: &Vec3, n: &Vec3) -> Vec3 {
     *v - *n * v.dot(*n) * 2.0
 }
 
+/// Returns a refracted ray direction using the given normal
+/// and the ratio between two refractive indices.
+/// 
+/// See [Shirley's RTiOW's section on Snell's Law](https://raytracing.github.io/books/RayTracingInOneWeekend.html#dielectrics/snell'slaw) for more information
 pub fn refract(uv: &Vec3, n: &Vec3, eta_ratio: f32) -> Vec3 {
     let cos_theta = (-*uv).dot(*n).min(1.0);
     let r_perp = eta_ratio * (*uv + cos_theta * *n);
