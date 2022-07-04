2f6dc13d7d7824523cc92d74c1fc731e6f892623
Nicolas Barrios <nbarrios1337@gmail.com>
2022-07-03 20:13:07 -0400

    Reorganize texure module

--- src/texture.rs
+++ src/texture.rs
@@ -8,12 +8,6 @@ pub trait Texture {
     fn color(&self, u: f32, v: f32, point: Vec3) -> Color;
 }
 
-impl Texture for Color {
-    fn color(&self, _u: f32, _v: f32, _point: Vec3) -> Color {
-        *self
-    }
-}
-
 impl Debug for dyn Texture {
     fn fmt(&self, f: &mut std::fmt::Formatter<'_>) -> std::fmt::Result {
         write!(
@@ -21,5 +15,11 @@ impl Debug for dyn Texture {
             "Texture with Color {:?}",
             self.color(0.0, 0.0, Vec3::ZERO)
         )
+    }
+}
+
+impl Texture for Color {
+    fn color(&self, _u: f32, _v: f32, _point: Vec3) -> Color {
+        *self
     }
 }
