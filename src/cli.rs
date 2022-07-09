beb9e078b92447e1dff76f96548979c4e88338ce
Nicolas Barrios <nbarrios1337@gmail.com>
2022-07-09 00:50:36 -0400

    Formatting

--- src/cli.rs
+++ src/cli.rs
@@ -17,6 +17,12 @@ pub struct Arguments {
     pub scene: SceneType,
 
     /// samples per pixel
-    #[clap(short='n', long, value_parser, default_value_t = 100, value_name = "NUM")]
+    #[clap(
+        short = 'n',
+        long,
+        value_parser,
+        default_value_t = 100,
+        value_name = "NUM"
+    )]
     pub samples_per_pixel: u32,
 }
