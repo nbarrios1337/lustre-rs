824e52055e991fcac2b46103b0ea334b087965e3
Nicolas Barrios <nbarrios1337@gmail.com>
2022-07-12 01:24:06 -0400

    Remove unused functions in random module

--- src/utils/random.rs
+++ src/utils/random.rs
@@ -6,40 +6,6 @@ use glam::Vec3;
 use rand::Rng;
 use rand_distr::{Distribution, UnitDisc, UnitSphere};
 
-/// Generates a random f32.
-pub fn rand_f32() -> f32 {
-    rand::thread_rng().gen::<f32>()
-}
-
-/// Generates a random usize.
-pub fn rand_usize() -> usize {
-    rand::thread_rng().gen::<usize>()
-}
-
-/// Generates a random f32 within the given range `[min, max)`.
-pub fn rand_range_f32(min: f32, max: f32) -> f32 {
-    rand::thread_rng().gen_range(min..max)
-}
-
-/// Generates a random usize within the given range `[min, max)`.
-pub fn rand_range_usize(min: usize, max: usize) -> usize {
-    rand::thread_rng().gen_range(min..max)
-}
-
-/// Generates a random [Vec3].
-pub fn rand_vec3() -> Vec3 {
-    rand::thread_rng().gen::<Vec3>()
-}
-
-/// Generates a random [Vec3] within the given range `[min, max)`.
-pub fn rand_range_vec3(min: f32, max: f32) -> Vec3 {
-    Vec3::new(
-        rand_range_f32(min, max),
-        rand_range_f32(min, max),
-        rand_range_f32(min, max),
-    )
-}
-
 /// Generates a random [Vec3] within the unit sphere (radius 1).
 ///
 /// wrapper function around [UnitSphere]'s sample method
