a575b84e3ea34671818f62009d031677c33055f0
Nicolas Barrios <nbarrios1337@gmail.com>
2022-07-19 22:15:48 -0400

    More documentation

--- src/render.rs
+++ src/render.rs
@@ -32,40 +32,60 @@ impl Renderer {
         let progress_bar = get_progressbar((self.image_height * self.image_width) as u64)
             .with_prefix("Generating pixels");
 
+        // Set up rendering properties
         let (cam, world) = scene;
-
-        // Generate image
         let depth = 50;
+
+        // Allocate image buffer
         let mut img_buf: image::RgbImage =
             image::ImageBuffer::new(self.image_width, self.image_height);
-        img_buf.enumerate_pixels_mut().par_bridge().for_each(|p| {
-            let (x, y, pixel) = p;
-            let mut color_v = (0..self.samples_per_pixel)
-                .into_par_iter()
-                .map_init(
-                    || SmallRng::from_rng(rand::thread_rng()),
-                    |rng, _| {
-                        let rng = rng.as_mut().unwrap();
-                        let offset_u: f32 = rng.gen();
-                        let offset_v: f32 = rng.gen();
-                        let u: f64 = (x as f32 + offset_u) as f64 / (self.image_width - 1) as f64;
-                        let v: f64 = ((self.image_height - y) as f32 + offset_v) as f64
-                            / (self.image_height - 1) as f64;
-                        let contrib = cam.get_ray(u as f32, v as f32, rng).shade(
-                            &world,
-                            depth,
-                            cam.bg_color,
-                            rng,
-                        );
-                        Vec3::from(contrib)
-                    },
-                )
-                .reduce(|| Vec3::ZERO, |a, b| a + b);
-            color_v /= self.samples_per_pixel as f32;
-            color_v = color_v.powf(0.5); // sqrt
-            progress_bar.inc(1);
-            *pixel = image::Rgb::<u8>::from(Color::new(color_v));
-        });
+
+        // Generate image
+        img_buf
+            .enumerate_pixels_mut()
+            .par_bridge()
+            .for_each(|indexed_pixel| {
+                // unpack the enumeration
+                let (x, y, pixel) = indexed_pixel;
+
+                // map reduce N samples into single Vec3
+                let mut color_v = (0..self.samples_per_pixel)
+                    .into_par_iter()
+                    .map_init(
+                        || SmallRng::from_rng(rand::thread_rng()),
+                        |rng, _| {
+                            // from_rng(...) gives Result, unpack here
+                            let rng = rng.as_mut().unwrap();
+                            // convert buffer indices to viewport coordinates
+                            let offset_u: f32 = rng.gen();
+                            let offset_v: f32 = rng.gen();
+                            let u: f64 =
+                                (x as f32 + offset_u) as f64 / (self.image_width - 1) as f64;
+                            let v: f64 = ((self.image_height - y) as f32 + offset_v) as f64
+                                / (self.image_height - 1) as f64;
+
+                            // trace ray
+                            let contrib = cam.get_ray(u as f32, v as f32, rng).shade(
+                                &world,
+                                depth,
+                                cam.bg_color,
+                                rng,
+                            );
+                            Vec3::from(contrib)
+                        },
+                    )
+                    .reduce(|| Vec3::ZERO, |a, b| a + b);
+
+                // Account for number of samples
+                color_v /= self.samples_per_pixel as f32;
+
+                // "gamma" correction
+                color_v = color_v.powf(0.5); // sqrt
+                progress_bar.inc(1);
+
+                // modify pixel with generated color value
+                *pixel = image::Rgb::<u8>::from(Color::new(color_v));
+            });
 
         progress_bar.finish_with_message("Done generating pixels");
 
